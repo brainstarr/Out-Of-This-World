999729232c10af8812cd8290f06c297e56ad680b
BrainStarr <brian.r.starr@gmail.com>
2014-10-01 19:30:32 -0700

    Added space data view and VC, some styling to the view

---
+++ out of this world/BSSpaceDataViewController.h
@@ -0,0 +1,14 @@
+//
+//  BSSpaceDataViewController.h
+//  out of this world
+//
+//  Created by Brian Starr on 10/1/14.
+//  Copyright (c) 2014 Brian Starr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BSSpaceDataViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+
+@end
